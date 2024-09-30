99e35612e05b58cef14ec8115ee4a0992b0832a1
Musab Bin Ubaid <joiya4636@gmail.com>
2024-09-29 23:39:25 -0700

    Update user.model.js

--- Full Stack YouTube Clone/src/models/user.model.js
+++ Full Stack YouTube Clone/src/models/user.model.js
@@ -1,64 +1,76 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-const userSchema =new mongoose.Schema({
-username:{
-type:String,
-required:true,
-unique:true,
-},
-email:{
-type:String,
-required:true,
-unique:true,
-trim:true
-},
-fullname:{
-type:String,   
-required:true,
-},
-avatar:{
-    type:String,
-    required:true,
-},
-coverImage:{
-    type:String
-},
-WatchHistory:[
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    WatchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Passoword is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+userSchema.methods.isPassowordcorrect = async function () {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateToken = function () {
+  return jwt.sign(
     {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Video"
+      _id: this.id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
     }
-],
-password:{
-    type:String,
-    required:[true,"Passoword is required"]
-},
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-},
-{
-    timestamps:true
-}
-
-)
-userSchema.pre("save",async function (next){
-    if(!this.isModified("password")) return next();
-this.password=bcrypt.hashSync(this.password,10);
-next();
-})
-userSchema.methods.isPassowordcorrect=async function(){
-    return await bcrypt.compare(password,this.password);
-}
-userSchema.methods.generateToken=function(){
-    
-    jwt.sign({
-        _id:this.id,
-        username:this.username,
-        email:this.email,
-        
-    })
-}
-userSchema.methods.generateRefrestToken=function(){
-    return jwt.sign({id:this._id},process.env.JWT_SECRET,{expiresIn:"7d"});
-}
-export const User=mongoose.model("User",userSchema);
+export const User = mongoose.model("User", userSchema);
