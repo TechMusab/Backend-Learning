2f64132872410a7313b6645304a11c981d303d5b
Musab Bin Ubaid <joiya4636@gmail.com>
2024-10-06 02:35:08 -0700

    Update index.js

--- Full Stack YouTube Clone/src/index.js
+++ Full Stack YouTube Clone/src/index.js
@@ -4,8 +4,8 @@ dotenv.config({
     path:"./env"
 })
 connectdb().then(()=>{
-app.listen(process.env.PORT || 8000,()=>
-    con{sole.log(`server is running on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is running on port ${process.env.PORT}`)
 })
 }).catch(err=>{
     console.log(err)
