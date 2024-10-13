6b6a639bec8a47b39a923645195a3a5754bd81cf
Musab Bin Ubaid <joiya4636@gmail.com>
2024-10-12 23:46:04 -0700

    Update app.js

--- First Node Server/app.js
+++ First Node Server/app.js
@@ -3,4 +3,7 @@ const http = require("http");
 const server=http.createServer((req, res) => {
   console.log(req);
 });
-server.listen(3000);
+const PORT=3000;
+server.listen(PORT,()=>{
+    console.log("listening on port ${PORT}");
+});
