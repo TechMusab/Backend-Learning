ec26c94b4b39c5f29f5bee32e3ad24662b806764
Musab Bin Ubaid <joiya4636@gmail.com>
2024-10-13 09:16:14 -0700

    Updated

--- First Node Server/app.js
+++ First Node Server/app.js
@@ -1,30 +1,52 @@
 const http = require("http");
-const PORT=3000;
-const server=http.createServer((req, res) => {
-  if(req.url==='/'){
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<body>Hi Client</body>');
-    res.write('</html>');
+const fs = require("fs");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  // if(req.url==='/'){
+  //   res.setHeader('Content-Type','text/html')
+  //   res.write('<html>')
+  //   res.write('<body>Hi Client</body>');
+  //   res.write('</html>');
+  //   res.end();
+  // }
+  // else if(req.url==='/products'){
+  //   res.setHeader('Content-Type','text/html')
+  //   res.write('<html>')
+  //   res.write('<body>Hi This is products</body>');
+  //   res.write('</html>');
+  //   res.end();
+  // }
+  // else{
+  //   res.setHeader('Content-Type','text/html')
+  //   res.write('<html>')
+  //   res.write('<body>Hi This is default</body>');
+  //   res.write('</html>');
+  //   res.end();
+  // }
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+  <html>
+    <body>
+      <h1>Sample Form</h1>
+      <form action="/submit" method="POST">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br><br>
+        
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br><br>
+        <input type="submit" value="Submit">
+      </form>
+    </body>
+  </html>
+`);
     res.end();
+  } else if (req.url === "/submit" && req.method === "POST") {
+    fs.writeFileSync("user.txt", "Musab Joiya");
+    res.statusCode=302
   }
-  else if(req.url==='/products'){
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<body>Hi This is products</body>');
-    res.write('</html>');
-    res.end();
-  }
-  else{
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<body>Hi This is default</body>');
-    res.write('</html>');
-    res.end();
-  }
-
 });
 
-server.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
