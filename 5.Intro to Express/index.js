d2be5317893db640f5e0b9d8533a7ccd17d69eed
Musab Bin Ubaid <joiya4636@gmail.com>
2024-10-15 07:56:55 -0700

    Update index.js

--- 5.Intro to Express/index.js
+++ 5.Intro to Express/index.js
@@ -1,16 +1,14 @@
-import http from 'http';
 import express from 'express';
 const app=express();
-const server=http.createServer(app)
-server.listen(3000,()=>{
-    console.log('server started');
-});
+app.listen(3000,()=>console.log('server started on http://localhost:3000')
+)
 //middleware
-app.use((req,res,next)=>{
+app.use("/",(req,res,next)=>{
     console.log('1st middleware');
     next();
 });
-app.use((req,res,next)=>{
+app.use("/submit",(req,res,next)=>{
     console.log('2nd middleware');
     res.send('<h1>hello</h1>');
 });
+//cannot call send after send
