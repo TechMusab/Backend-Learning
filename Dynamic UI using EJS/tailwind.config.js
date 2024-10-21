18c959c26d2db99145b11b9190f51c526f5bfba6
Musab Bin Ubaid <joiya4636@gmail.com>
2024-10-21 09:38:35 -0700

    updated

--- Dynamic UI using EJS/tailwind.config.js
+++ Dynamic UI using EJS/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*/*.{html,js}"],
+  content: ["./**/*.{html,js}"],  // This will scan all .html and .js files in the entire project directory.
   theme: {
     extend: {},
   },
